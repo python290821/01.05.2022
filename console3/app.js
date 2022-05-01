7fe872974015996ea9c17fc589845c4436802ef2
python290821 <python290821@gmail.com>
2022-05-01 19:51:49 +0300

    upload console3

---
+++ console3/app.js
@@ -0,0 +1,24 @@
+//console.log(module);
+
+console.log(__dirname);
+
+const path = require('path')
+console.log(path.join(__dirname, 'myfile.txt'));
+
+const os = require('os');
+console.log(`Free memory: ${os.freemem()}`);
+console.log(`Free memory: ${os.totalmem()}`);
+
+const fs = require('fs')
+const files = fs.readdirSync('./')
+console.log(files);
+
+fs.readdir(__dirname + '____________', (err, files) => {
+    if (err)
+        console.error(`[async] Error ${err}`);
+    else {
+        console.log(`[async] Files: `);
+        console.log(files);
+    }
+})
+//
